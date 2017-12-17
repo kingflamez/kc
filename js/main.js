7877deff767f07bba9ebf23085be9c9d96f92b68
Flamez <flamekeed@gmail.com>
2017-12-17 18:19:07 +0100

    Added Company

--- js/main.js
+++ js/main.js
@@ -2,17 +2,18 @@ function navigate() {
 	console.log(document.getElementById("mySidenav").style.width);
 	if (document.getElementById("mySidenav").style.width == "250px") {
 		document.getElementById("mySidenav").style.width = "0";
-    	document.getElementById("content").style.transform = "translate(0)";
+    	document.getElementById("content").style.transform = "none";
 	}
 
 	else{
 	    document.getElementById("mySidenav").style.width = "250px";
 	    document.getElementById("content").style.transform = "translate(-250px)";
+    	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 	}
 }
 $(".my-rating").starRating({
     starSize: 18,
-    initialRating: 4.5,
+    initialRating: 4.3,
     readOnly: true,
     callback: function(currentRating, $el){
         // make a server call here
